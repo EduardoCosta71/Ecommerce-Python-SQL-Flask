c2f93bea758a0811f10ae3042e2a0e347738f280
EduardoCosta71 <duboy42@gmail.com>
2026-07-17 20:01:00 -0300

    Atualização - Area  Checkout

--- ecommerce/routes/pedidos.py
+++ ecommerce/routes/pedidos.py
@@ -12,7 +12,40 @@ def get_db_connection():
 
 def pedidos_registrar(app):
 
-    @app.route('/pedidos')
-    def pedidos():
+    @app.route('/checkout')
+    def checkout():
+
+        if 'usuario_id' not in session:
+            return redirect(url_for('usuarios'))
         
-        return "Lista de pedidos"
+        usuario_id = session['usuario_id']
+        
+        conn = get_db_connection()
+        cursor = conn.cursor()
+
+        cursor.execute(""" SELECT
+                   IC.Id,
+                   P.Id,
+                   P.Nome,
+                   P.Preco,
+                   P.Imagem,
+                   IC.Quantidade,
+                   (P.Preco * IC.Quantidade) AS Subtotal
+                FROM ItensCarrinho IC
+                INNER JOIN Produtos P 
+                ON IC.ProdutoId = P.Id
+                INNER JOIN Carrinhos C
+                ON IC.CarrinhoId = C.Id
+                WHERE C.UsuarioId = ?
+                """, (usuario_id,))
+    
+        itens = cursor.fetchall()
+
+        total = 0
+
+        for item in itens:
+            total += item.Subtotal
+
+        conn.close()
+
+        return render_template("checkout/checkout.html", itens=itens, total=total)
