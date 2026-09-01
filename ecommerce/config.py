02336fd11050a21f184f494bd2f921418d2f6236
EduardoCosta71 <duboy42@gmail.com>
2026-09-01 19:13:39 -0300

    Atualização banco de dados

---
+++ ecommerce/config.py
@@ -0,0 +1,10 @@
+import pyodbc
+
+def get_db_connection():
+    #Função para estabelecer conexão com o banco de dados SQL Server
+    return pyodbc.connect(
+        'DRIVER={SQL Server};'
+        'SERVER=localhost\\SQLEXPRESS;'  # Substitua pelo nome do seu servidor e instância
+        'DATABASE=Ecommerce;'  # Substitua pelo nome do seu banco de dados
+        'Trusted_Connection=yes;'
+    )
--- ecommerce/routes/produtos.py
+++ ecommerce/routes/produtos.py
@@ -1,14 +1,6 @@
 from flask import Blueprint, render_template, request, redirect, url_for, session
 import pyodbc
-
-def get_db_connection():
-    #Função para estabelecer conexão com o banco de dados SQL Server
-    return pyodbc.connect(
-        'DRIVER={SQL Server};'
-        'SERVER=localhost\\SQLEXPRESS;'  # Substitua pelo nome do seu servidor e instância
-        'DATABASE=Ecommerce;'  # Substitua pelo nome do seu banco de dados
-        'Trusted_Connection=yes;'
-    )
+from config import get_db_connection
 
 def produtos_registrar(app):
 
